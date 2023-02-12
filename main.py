8012bf8d5fd25599e901f52b549098974a69ff75
Harshavii <112164150+Harshavii@users.noreply.github.com>
2023-02-12 11:41:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 import datetime as dt
 now = dt.datetime.now()
 day = now.weekday()
-email_id = "lily21042002@gmail.com"
-password = "lfcseehyhcqgjhzh"
+email_id = "SENDER_EMAIL_ID"
+password = "GENERATED_PASSWORD"
 if day>=0 and day<=6:
       with open("quotes.txt") as file:
           all_quotes = file.readlines()
@@ -14,5 +14,5 @@ if day>=0 and day<=6:
       with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(user=email_id,password=password)
-            connection.sendmail(from_addr=email_id,to_addrs="harshavibodkhe@gmail.com",
+            connection.sendmail(from_addr=email_id,to_addrs="RECEIVER_EMAIL_ID",
                                 msg=f"Subject:Today's Wisdom :)\n\n{quote}")
